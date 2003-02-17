1236e8461a6e15f3701a8e42a1f7d09ac4f959c2
Laci Ladanyi <ladanyi@us.ibm.com>
2003-02-17 04:12:35 +0000

    xlC related changes

--- include/BCP_aix43.hpp
+++ include/BCP_aix43.hpp
@@ -10,18 +10,8 @@
 #include <unistd.h>       // for setpriority() and gethostname()
 typedef int BCP_IndexType;
 
-#if defined(__GNUC__)
+#if defined(__IBMCPP__) && (__IBMCPP__ >= 5)
 #  define BCP_DEFAULT_NAMESPACE
-
-#  define NEED_IMPLICIT_TEMPLATE_FUNCTIONS 1
-#  define NEED_IMPLICIT_TEMPLATE_CLASSES 1
-#  define BCP_PtrDiff       long
-#  define BCP_CONSTRUCT     std::construct
-#  define BCP_DESTROY       std::destroy
-#  define BCP_DESTROY_RANGE std::destroy
-
-#elif defined(__IBMCPP__) && (__IBMCPP__ >= 5)
-
 #  define BCP_PtrDiff       long
 #  define BCP_CONSTRUCT     std::_Construct
 #  define BCP_DESTROY       std::_Destroy
@@ -30,6 +20,44 @@ typedef int BCP_IndexType;
              do {				\
                 std::_Destroy(--last);		\
 	     } while (first != last);
+#endif
+
+#if defined(__GNUC__)
+#  define BCP_DEFAULT_NAMESPACE
+#  include <sys/resource.h> // for setpriority()
+#  ifndef __USE_BSD
+#    define __USE_BSD  // to get gethostname() from unistd.h
+#    include <unistd.h>
+#    undef __USE_BSD
+#  else
+#    include <unistd.h>
+#  endif
+#  define BCP_PtrDiff       int
+
+#  if (__GNUC__ >= 3)
+
+#    define NEED_TEMPLATE_CLASSES
+#    define NEED_TEMPLATE_FUNCTIONS
+// #    define NEED_STD_TEMPLATE_FUNCTIONS
+// #    define NEED_IMPLICIT_TEMPLATE_CLASSES
+// #    define NEED_IMPLICIT_TEMPLATE_FUNCTIONS
+#    define BCP_CONSTRUCT     std::_Construct
+#    define BCP_DESTROY       std::_Destroy
+#    define BCP_DESTROY_RANGE std::_Destroy
+
+#  else
+
+#    define NEED_TEMPLATE_CLASSES
+#    define NEED_TEMPLATE_FUNCTIONS
+// #    define NEED_STD_TEMPLATE_FUNCTIONS
+// #    define NEED_IMPLICIT_TEMPLATE_CLASSES
+// #    define NEED_IMPLICIT_TEMPLATE_FUNCTIONS
+#    define BCP_CONSTRUCT     std::construct
+#    define BCP_DESTROY       std::destroy
+#    define BCP_DESTROY_RANGE std::destroy
+
+#  endif
+
 #endif
 
 #endif
