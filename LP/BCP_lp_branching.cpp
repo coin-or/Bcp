2e7c540bb0d84ece0e035e6ec917a4c04f233976
Laci Ladanyi <ladanyi@us.ibm.com>
2003-10-14 01:48:41 +0000

    fixed using a possibly outdated LP solver

--- LP/BCP_lp_branching.cpp
+++ LP/BCP_lp_branching.cpp
@@ -397,7 +397,6 @@ static inline BCP_branching_decision
 BCP_lp_select_branching_object(BCP_lp_prob& p,
 			       BCP_presolved_lp_brobj*& best_presolved)
 {
-   OsiSolverInterface* lp = p.lp_solver;
    BCP_var_set& vars = p.node->vars;
    BCP_cut_set& cuts = p.node->cuts;
    BCP_vec<BCP_lp_branching_object*> candidates;
@@ -490,8 +489,8 @@ LP: Strong branching is disabled but more than one candidate is selected.\n\
    
    // Now just resolve the LP to get what'll be sent to the TM.
    p.user->modify_lp_parameters(p.lp_solver, false);
-   lp->resolve();
-   p.lp_result->get_results(*lp);
+   p.lp_solver->initialSolve();
+   p.lp_result->get_results(*p.lp_solver);
    p.node->quality = p.lp_result->objval();
 
    p.stat.time_branching += BCP_time_since_epoch() - time0;
