cc35a9dbf599133139029211be1f94b1755f2269
Laci Ladanyi <ladanyi@us.ibm.com>
2006-07-06 05:19:51 +0000

    split up the MCF example into several files

---
+++ Bcp/examples/MCF/include/MCF_data.hpp
@@ -0,0 +1,48 @@
+#ifndef MCF_data_hpp
+#define MCF_data_hpp
+
+#include <iostream>
+#include "BCP_buffer.hpp"
+
+//#############################################################################
+
+// This structure holds the input data
+
+class MCF_data {
+public:
+    struct arc {
+	int tail;
+	int head;
+	int lb;
+	int ub;
+	double weight;
+    };
+    struct commodity {
+	int source;
+	int sink;
+	int demand;
+    };
+    char* problem_name;
+    arc* arcs;
+    commodity* commodities;
+    int numarcs;
+    int numnodes;
+    int numcommodities;
+
+public:
+    MCF_data() :
+	arcs(NULL), commodities(NULL),
+	numarcs(0), numnodes(0), numcommodities(0) {}
+
+    ~MCF_data() {
+	delete[] arcs;
+	delete[] commodities;
+	delete[] problem_name;
+    }
+
+    int readDimacsFormat(std::istream& s, bool addDummyArcs);
+    void pack(BCP_buffer& buf) const;
+    void unpack(BCP_buffer& buf);
+};
+
+#endif
