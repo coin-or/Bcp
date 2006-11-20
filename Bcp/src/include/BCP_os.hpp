ed33976317814ffa0c3c92b0a84fe4064c413957
Laci Ladanyi <ladanyi@us.ibm.com>
2006-11-20 20:14:22 +0000

    Added checking of memory left in case a sysinfo() call is available

--- Bcp/src/include/BCP_os.hpp
+++ Bcp/src/include/BCP_os.hpp
@@ -3,24 +3,40 @@
 #ifndef _BCP_OS_H
 #define _BCP_OS_H
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 #if HAVE_SYS_RESOURCE_H
 #  include <sys/resource.h>
 #else
 #  define setpriority(x,y,z)
 #endif
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 #if HAVE_UNISTD_H
 #  include <unistd.h>
 #endif
+
 #ifndef HAVE_GETHOSTNAME
 #  define gethostname(x,y)
 #endif
+
 #if HAVE_GETPID
 #  define GETPID (getpid())
 #else
 #  define GETPID (0)
 #endif
-//------------------------------------------------------------------------------
+
+/** Return the amount of memory left in kilobytes. Note that this works only
+    if sysinfo() is available, otherwise returns -1. */
+#ifdef HAVE_SYSINFO
+#include <sys/sysinfo.h>
+static inline int freemem() {
+    struct sysinfo info;
+    sysinfo(&info);
+    return (info.mem_unit*info.freeram)/1024;
+}
+#else
+static inline int freemem() {
+    return -1;
+}
+#endif
 
 #endif
