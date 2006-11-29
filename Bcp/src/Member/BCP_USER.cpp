0b15c6386154022bc9485bfe1843b90ae3862819
Laci Ladanyi <ladanyi@us.ibm.com>
2006-11-29 04:24:16 +0000

    still working on MPI

--- Bcp/src/Member/BCP_USER.cpp
+++ Bcp/src/Member/BCP_USER.cpp
@@ -1,16 +1,14 @@
 // Copyright (C) 2000, International Business Machines
 // Corporation and others.  All Rights Reserved.
+#include "BcpConfig.h"
+
 #include "BCP_error.hpp"
 bool BCP_fatal_error::abort_on_error = true;
 
 #include "BCP_USER.hpp"
 #include "BCP_message_single.hpp"
-#if defined(COIN_HAS_MPI)
 #include "BCP_message_mpi.hpp"
-#endif
-#if defined(COIN_HAS_PVM)
 #include "BCP_message_pvm.hpp"
-#endif
 
 //-----------------------------------------------------------------------------
 
@@ -18,7 +16,7 @@ BCP_message_environment *
 USER_initialize::msgenv_init(int argc, char* argv[]) {
     int procid = -1; // assume no parallel environment
 #if defined(COIN_HAS_MPI)
-    procid = BCP_mpi_myid();
+    procid = BCP_is_mpi(argc, argv);
     if (procid >= 0) {
 	return new BCP_mpi_environment(argc, argv);
     }
