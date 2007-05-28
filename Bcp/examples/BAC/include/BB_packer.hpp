46dc3bf21a9753ca3042e67a0555d103e9f6e01d
Francois Margot <fmargot@andrew.cmu.edu>
2007-05-28 13:12:25 +0000

    Cosmetics

--- Bcp/examples/BAC/include/BB_packer.hpp
+++ Bcp/examples/BAC/include/BB_packer.hpp
@@ -1,4 +1,4 @@
-// Last edit: 5/18/07
+// Last edit: 5/20/07
 //
 // Name:     BB_packer.hpp
 // Author:   Francois Margot
@@ -21,59 +21,60 @@ class BB_user_data;
 
 class BB_packer : public BCP_user_pack {
 public:
-    //-------------------------------------------------------------------------
-    /** Pack warmstarting information */
-    virtual void
-    pack_warmstart(const BCP_warmstart* ws, BCP_buffer& buf,
+  
+  //-------------------------------------------------------------------------
+  /** Pack warmstarting information */
+  virtual void
+  pack_warmstart(const BCP_warmstart* ws, BCP_buffer& buf,
+		 bool report_if_default = false)
+  {
+    if (report_if_default) {
+      printf("BCP_user_pack : default pack_warmstart() executed.\n");
+    }
+    BCP_pack_warmstart(ws, buf);
+  }
+  
+  /** Unpack warmstarting information */
+  virtual BCP_warmstart*
+  unpack_warmstart(BCP_buffer& buf,
 		   bool report_if_default = false)
-    {
-	if (report_if_default) {
-	    printf("BCP_user_pack : default pack_warmstart() executed.\n");
-	}
-	BCP_pack_warmstart(ws, buf);
+  {
+    if (report_if_default) {
+      printf("BCP_user_pack : default unpack_warmstart() executed.\n");
     }
-
-    /** Unpack warmstarting information */
-    virtual BCP_warmstart*
-    unpack_warmstart(BCP_buffer& buf,
-		     bool report_if_default = false)
-    {
-	if (report_if_default) {
-	    printf("BCP_user_pack : default unpack_warmstart() executed.\n");
-	}
-	return BCP_unpack_warmstart(buf);
-    }
-    
-    //-------------------------------------------------------------------------
-    /** Pack an algorithmic variable */
-    virtual void
-    pack_var_algo(const BCP_var_algo* var, BCP_buffer& buf)
-    {
-	throw BCP_fatal_error("\
+    return BCP_unpack_warmstart(buf);
+  }
+  
+  //-------------------------------------------------------------------------
+  /** Pack an algorithmic variable */
+  virtual void
+  pack_var_algo(const BCP_var_algo* var, BCP_buffer& buf)
+  {
+    throw BCP_fatal_error("\
 BCP_user_pack::pack_var_algo() invoked but not overridden!\n");
-    }
-
-    /** Unpack an algorithmic variable */
-    virtual BCP_var_algo* unpack_var_algo(BCP_buffer& buf)
-    {
-	throw BCP_fatal_error("\
+  }
+  
+  /** Unpack an algorithmic variable */
+  virtual BCP_var_algo* unpack_var_algo(BCP_buffer& buf)
+  {
+    throw BCP_fatal_error("\
 BCP_user_pack::unpack_var_algo() invoked but not overridden!\n");
-	return 0; // to satisfy aCC on HP-UX
-    }
-      
-    //-------------------------------------------------------------------------
-    /** Pack an algorithmic cut */
-    virtual void pack_cut_algo(const BCP_cut_algo* cut, BCP_buffer& buf);
-
-    /** Unpack an algorithmic cut */
-    virtual BCP_cut_algo* unpack_cut_algo(BCP_buffer& buf);
-
-    //-------------------------------------------------------------------------
-    /** Pack an user data */
-    virtual void pack_user_data(const BCP_user_data* ud, BCP_buffer& buf);
-
-    /** Unpack an user data */
-    virtual BCP_user_data* unpack_user_data(BCP_buffer& buf);
+    return 0; // to satisfy aCC on HP-UX
+  }
+  
+  //-------------------------------------------------------------------------
+  /** Pack an algorithmic cut */
+  virtual void pack_cut_algo(const BCP_cut_algo* cut, BCP_buffer& buf);
+  
+  /** Unpack an algorithmic cut */
+  virtual BCP_cut_algo* unpack_cut_algo(BCP_buffer& buf);
+  
+  //-------------------------------------------------------------------------
+  /** Pack an user data */
+  virtual void pack_user_data(const BCP_user_data* ud, BCP_buffer& buf);
+  
+  /** Unpack an user data */
+  virtual BCP_user_data* unpack_user_data(BCP_buffer& buf);
 };
 
 
